ab869104d70bcc21d4b9b1f095bad5729fbd8579
Beomman-Han <qjaaks6378@gmail.com>
2022-05-02 23:09:29 +0900

    Add Queue class for bfs algorithm
    Queue class is implemented by python deque and generic...

--- algorithm/classic_problem/ch2_search.py
+++ algorithm/classic_problem/ch2_search.py
@@ -214,4 +214,19 @@ def node_to_path(node: Node[T]) -> List[T]:
     return path
 
 
+class Queue(Generic[T]):
+    def __init__(self) -> None:
+        self._container: Deque[T] = deque()
     
+    @property
+    def empty(self) -> bool:
+        return not self._container
+    
+    def push(self, item: T) -> None:
+        self._container.append(item)
+    
+    def pop(self) -> T:
+        return self._container.popleft()
+    
+    def __repr__(self) -> str:
+        return repr(self._container)
