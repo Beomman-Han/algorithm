0e0e9715c07cb31c89a4b4d343384d632de3d3cc
Beomman-Han <qjaaks6378@gmail.com>
2022-05-02 22:48:42 +0900

    Test dfs algorithm implemented with Maze
    Print path from start to goal position found by dfs algorithm.

--- algorithm/classic_problem/test_ch2_search.py
+++ algorithm/classic_problem/test_ch2_search.py
@@ -6,7 +6,17 @@ if __name__ == '__main__':
     maze : Maze = Maze()
     print(maze)
     
-    print(maze.goal_test(MazeLocation(4, 5)))  ## False
-    print(maze.goal_test(MazeLocation(9, 9)))  ## True
+    #print(maze.goal_test(MazeLocation(4, 5)))  ## False
+    #print(maze.goal_test(MazeLocation(9, 9)))  ## True
+    #print(maze.successors(MazeLocation(0, 0)))
+    solution_1: Optional[Node[MazeLocation]] = dfs(
+        maze.start, maze.goal_test, maze.successors)
     
-    print(maze.successors(MazeLocation(0, 0)))
+    if solution_1 is None:
+        print('No path found by dfs algorithm')
+    else:
+        path_1: List[MazeLocation] = node_to_path(solution_1)
+        maze.mark(path_1)
+        print(maze)
+        maze.clear(path_1)
+    
