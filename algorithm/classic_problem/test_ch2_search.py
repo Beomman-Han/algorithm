9639f32eff11ce7fa07f3baf0becc9ac701c344a
Beomman-Han <qjaaks6378@gmail.com>
2022-05-02 23:30:23 +0900

    Test bfs algorithm implemented with Maze

--- algorithm/classic_problem/test_ch2_search.py
+++ algorithm/classic_problem/test_ch2_search.py
@@ -9,6 +9,8 @@ if __name__ == '__main__':
     #print(maze.goal_test(MazeLocation(4, 5)))  ## False
     #print(maze.goal_test(MazeLocation(9, 9)))  ## True
     #print(maze.successors(MazeLocation(0, 0)))
+    
+    ## Test dfs algorithm
     solution_1: Optional[Node[MazeLocation]] = dfs(
         maze.start, maze.goal_test, maze.successors)
     
@@ -19,4 +21,15 @@ if __name__ == '__main__':
         maze.mark(path_1)
         print(maze)
         maze.clear(path_1)
+    
+    ## Test bfs algorithm
+    solution_2: Optional[Node[MazeLocation]] = bfs(
+        maze.start, maze.goal_test, maze.successors)
+    if solution_2 is None:
+        print('No path found by bfs algorithm')
+    else:
+        path_2: List[MazeLocation] = node_to_path(solution_2)
+        maze.mark(path_2)
+        print(maze)
+        maze.clear(path_2)
     
