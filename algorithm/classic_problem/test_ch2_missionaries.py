47c848ce8b988abdcbbe17978bbb15ae7d46f9ed
Beomman-Han <qjaaks6378@gmail.com>
2022-05-04 18:42:29 +0900

    Find solution of missionaries problem

--- algorithm/classic_problem/test_ch2_missionaries.py
+++ algorithm/classic_problem/test_ch2_missionaries.py
@@ -1,4 +1,5 @@
 from ch2_missionaries import *
+from ch2_search import bfs, Node, node_to_path
 
 if __name__ == "__main__":
     
@@ -13,4 +14,12 @@ if __name__ == "__main__":
     mc_state = MCState(3, 1, False)
     possible_scenarios = mc_state.successors()
     for scenario in possible_scenarios:
+        print(scenario)
+    
+    ## solve problem
+    mc_state = MCState(3, 3, True)
+    #solution = bfs(mc_state, mc_state.goal_test, mc_state.successors)
+    solution = bfs(mc_state, MCState.goal_test, MCState.successors)
+    path = node_to_path(solution)
+    for scenario in path:
         print(scenario)
