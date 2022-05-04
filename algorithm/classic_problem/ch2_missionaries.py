c68d7ac1e2495cddd245aacd7ef2eb5ba21bf27b
Beomman-Han <qjaaks6378@gmail.com>
2022-05-04 16:44:12 +0900

    Add __str__ method for printing state

--- algorithm/classic_problem/ch2_missionaries.py
+++ algorithm/classic_problem/ch2_missionaries.py
@@ -4,10 +4,13 @@ from __future__ import annotations
 MAX_NUM: int = 3
 
 class MCState:
-    """Missionaries and cannibals are at the east of river.
-    The object is to move all missionaries from the east to the west safely.
-    If the number of missionaries is smaller than cannibals,
-    cannibals would eat missionaries...
+    """Missionaries problem
+    1) Missionaries and cannibals are at the east of river.
+    2) They have a boat where maximum two people can aboard.
+    3) They should move from the west to the east safely.
+    4) If the number of missionaries is smaller than cannibals,
+    cannibals could eat missionaries.
+    
     This is for solving missionaries and cannibals problem with path search algorithm.
     """
     
@@ -22,3 +25,9 @@ class MCState:
         self.em = MAX_NUM - missionaries  ## missionaries at east
         self.ec = MAX_NUM - cannibals  ## cannibals at east
         self.boat = boat
+    
+    def __str__(self) -> str:
+        ret_str = f'{self.wm} missionaries and {self.wc} cannibals at west.\n'
+        ret_str += f'{self.em} missionaries and {self.ec} cannibals at east.\n'
+        ret_str += f'boat are at {("west" if self.boat else "east")}'
+        return ret_str
