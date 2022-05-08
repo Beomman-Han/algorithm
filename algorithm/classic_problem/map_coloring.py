5b123e8f165bc5042653c63b932522ab8b5833e1
Beomman-Han <qjaaks6378@gmail.com>
2022-05-08 23:31:23 +0900

    Solve map-color problem of australia with CSP module

--- algorithm/classic_problem/map_coloring.py
+++ algorithm/classic_problem/map_coloring.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List, Optional
 from csp import Constraint, CSP
 
 class MapColoringConstraint(Constraint[str, str]):
@@ -23,3 +23,32 @@ class MapColoringConstraint(Constraint[str, str]):
         if self.place1 not in assignment or self.place2 not in assignment:
             return True
         return assignment[self.place1] != assignment[self.place2]
+
+if __name__ == "__main__":
+    ## solve color mapping problem of AUS...
+    ## with CSP and MapColoringConstraint object
+    
+    variables : List[str] = ['웨스턴 오스트레일리아', '노던 준주', '사우스 오스트레일리아', 
+                           '퀸즐랜드', '뉴 사우스웨일스', '빅토리아', '태즈메이니아']
+    domains : Dict[str, List[str]] = {region : ['빨강', '초록', '파랑'] for region in variables}
+    
+    csp : CSP[str, str] = CSP(variables, domains)
+
+    csp.add_constraint(MapColoringConstraint('웨스턴 오스트레일리아', '노던 준주'))
+    csp.add_constraint(MapColoringConstraint('웨스턴 오스트레일리아', '사우스 오스트레일리아'))
+    
+    csp.add_constraint(MapColoringConstraint('노던 준주', '사우스 오스트레일리아'))
+    csp.add_constraint(MapColoringConstraint('노던 준주', '퀸즐랜드'))
+    
+    csp.add_constraint(MapColoringConstraint('사우스 오스트레일리아', '퀸즐랜드'))
+    csp.add_constraint(MapColoringConstraint('사우스 오스트레일리아', '뉴 사우스웨일스'))
+    csp.add_constraint(MapColoringConstraint('사우스 오스트레일리아', '빅토리아'))
+    
+    csp.add_constraint(MapColoringConstraint('퀸즐랜드', '뉴 사우스웨일스'))
+    
+    csp.add_constraint(MapColoringConstraint('뉴 사우스웨일스', '빅토리아'))
+    
+    csp.add_constraint(MapColoringConstraint('빅토리아', '태즈메이니아'))
+    
+    solution : Optional[Dict[str, str]] = csp.backtracking_search()
+    print(solution)
