9f374faf32867419d5c6d8cae6b97fefeb0a1bd8
Beomman-Han <qjaaks6378@gmail.com>
2022-05-15 16:20:03 +0900

    Add getter method for attributes with property

--- algorithm/classic_problem/ch4_graph.py
+++ algorithm/classic_problem/ch4_graph.py
@@ -20,3 +20,13 @@ class Graph(Generic[V]):
     def __init__(self, vertices : List[V] = []) -> None:
         self._vertices = vertices
         self._edges = List[List[Edge]] = [[] for _ in vertices]
+    
+    @property
+    def vertex_count(self) -> int:
+        return len(self._vertices)
+    
+    @property
+    def edge_count(self) -> int:
+        return sum(map(len, self._edges))
+    
+    
