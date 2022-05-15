4dc40dce45f7b5f07fbd2dd55ba95aadf0389d7c
Beomman-Han <qjaaks6378@gmail.com>
2022-05-15 16:09:35 +0900

    Add Graph class at ch4_graph.py
    Graph class designed with Generic type for broadly using.
    It has 2 attributes, self._vertices and self._edges.

--- algorithm/classic_problem/ch4_graph.py
+++ algorithm/classic_problem/ch4_graph.py
@@ -1,14 +1,22 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Generic, List, TypeVar
 
 
 @dataclass
 class Edge:
-    u: int  ## from vertex u
-    v: int  ## to vertex v
+    u: int  ## from vertex u, index of vertex
+    v: int  ## to vertex v, index of vertex
 
     def reversed(self) -> Edge:
         return Edge(self.v, self.u)
     
     def __str__(self) -> str:
         return f'{self.u} -> {self.v}'
+
+V = TypeVar('V')
+
+class Graph(Generic[V]):
+    def __init__(self, vertices : List[V] = []) -> None:
+        self._vertices = vertices
+        self._edges = List[List[Edge]] = [[] for _ in vertices]
