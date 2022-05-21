df205dc121d3e2824ceafecbeafe6b18d4759eda
Beomman-Han <qjaaks6378@gmail.com>
2022-05-21 12:20:50 +0900

    Start to implement dijkstra algorithm
    Add DijkstraNode which contains index of vertex and distance.

---
+++ algorithm/classic_problem/dijkstra.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from typing import TypeVar
+
+V = TypeVar('V')
+
+
+@dataclass
+class DijkstraNode:
+    vertex : int
+    distance : float
+    
+    def __lt__(self, other : DijkstraNode) -> bool:
+        return self.distance < other.distance
+    
+    def __eq__(self, other : DijkstraNode) -> bool:
+        return self.distance == other.distance
