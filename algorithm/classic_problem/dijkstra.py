d16439f15c0b9a23815d48452659424b447baea8
Beomman-Han <qjaaks6378@gmail.com>
2022-05-23 23:21:53 +0900

    Add Dijkstra algorithm function

--- algorithm/classic_problem/dijkstra.py
+++ algorithm/classic_problem/dijkstra.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import TypeVar
+from queue import PriorityQueue
+from typing import Dict, List, Optional, Tuple, TypeVar
+import sys
+sys.path.insert(0, '.')
+from ch4_graph import WeightedGraph, WeightedEdge
 
 V = TypeVar('V')
 
@@ -15,3 +19,30 @@ class DijkstraNode:
     
     def __eq__(self, other : DijkstraNode) -> bool:
         return self.distance == other.distance
+
+
+def dijkstra(
+    wg : WeightedGraph[V],
+    root : V
+    ) -> Tuple[List[Optional[float]],
+               Dict[int, WeightedEdge]]:
+    
+    first : int = wg.index_of(root)
+    distances : List[Optional[float]] = [None] * wg.vertex_count
+    distances[first] = 0
+    path_dict : Dict[int, WeightedEdge] = {}
+    pq : PriorityQueue[DijkstraNode] = PriorityQueue()
+    pq.push(DijkstraNode(first, 0))
+    
+    while not pq.empty:
+        u : int = pq.pop().vertex
+        dist_u : float = distances[u]
+        
+        for we in wg.edges_for_index(u):
+            dist_v : float = distances[we.v]
+            if dist_v is None or dist_v > we.weight + dist_u:
+                distances[we.v] = we.weight + dist_u
+                path_dict[we.v] = we
+                pq.push(DijkstraNode(we.v, we.weight + dist_u))
+    
+    return distances, path_dict
