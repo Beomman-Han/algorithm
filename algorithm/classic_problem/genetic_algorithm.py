b287a76f7e63fa7b36fb94bf7f36304db5557764
Beomman-Han <qjaaks6378@gmail.com>
2022-05-27 22:27:59 +0900

    Add _reproduce_and_replace() method to make new population

--- algorithm/classic_problem/genetic_algorithm.py
+++ algorithm/classic_problem/genetic_algorithm.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 from heapq import nlargest
-from random import choices
+from random import choices, random
 from typing import Callable, Generic, List, Tuple, Type, TypeVar
 
 
@@ -52,3 +52,25 @@ class GeneticAlgorithm(Generic[C]):
         ## pick chromosomes of num_participants randomly
         participants : List[C] = choices(self._population, k=num_participants)
         return tuple(nlargest(2, participants, key=self._fitness_key))
+    
+    def _reproduce_and_replace(self) -> None:
+        ## replace existing population to new population
+        new_population : List[C] = []
+        
+        while len(new_population) < len(self._population):
+            if self._selection_type == GeneticAlgorithm.SelectionType.ROULETTE:
+                parents : Tuple[C, C] = self._pick_roulette(
+                    [x.fitness() for x in self._population])
+            else:
+                parents : Tuple[C, C] = self._pick_tournament(
+                    len(self._population) // 2)
+            ## crossover
+            if random() < self._crossover_chance:
+                new_population.extend(parents[0].crossover(parents[1]))
+            else:
+                new_population.extend(parents)
+        ## If num of population is odd, num of new population must be 
+        ## larger than previous population.
+        if len(new_population) > len(self._population):
+            new_population.pop()
+        self._population = new_population
