31e117cb65092936872b40934fabf726c8ca0db2
Beomman-Han <qjaaks6378@gmail.com>
2022-06-08 21:49:17 +0900

    Add dot_product() func at ann.py

--- algorithm/classic_problem/ann.py
+++ algorithm/classic_problem/ann.py
@@ -1,1 +1,11 @@
 """This is for artificial neural network practice."""
+
+from typing import List
+from math import exp
+
+
+def dot_product(
+    xs : List[float],
+    ys : List[float]
+    ) -> float:
+    return sum([x * y for x, y in zip(xs, ys)])
