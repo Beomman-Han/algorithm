8a15a3ec6aea8f14b2abe0dc4511fa5f2faaad0e
Beomman-Han <qjaaks6378@gmail.com>
2022-06-08 21:51:56 +0900

    Add derivative_sigmoid() func for back propagation of ann

--- algorithm/classic_problem/ann.py
+++ algorithm/classic_problem/ann.py
@@ -12,3 +12,7 @@ def dot_product(
 
 def sigmoid(x : float) -> float:
     return 1.0 / (1.0 + exp(-x))
+
+def derivative_sigmoid(x : float) -> float:
+    sig : float = sigmoid(x)
+    return sig * (1 - sig)
