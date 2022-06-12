3724e214e1421d7903ecb1e48b1b11b1f593a241
Beomman-Han <qjaaks6378@gmail.com>
2022-06-12 13:14:17 +0900

    Add Neuron class at ann.py
    Neuron class has only __init__() and output() method.

--- algorithm/classic_problem/ann.py
+++ algorithm/classic_problem/ann.py
@@ -1,6 +1,6 @@
 """This is for artificial neural network practice."""
 
-from typing import List
+from typing import Callable, List
 from math import exp
 
 
@@ -16,3 +16,23 @@ def sigmoid(x : float) -> float:
 def derivative_sigmoid(x : float) -> float:
     sig : float = sigmoid(x)
     return sig * (1 - sig)
+
+
+class Neuron:
+    def __init__(self,
+        weights : List[float],
+        learning_rate : float,
+        activation_function : Callable[[float], float],
+        derivative_activation_function : Callable[[float], float]
+        ) -> None:
+        
+        self.weights = weights
+        self.activation_function = activation_function
+        self.derivative_activation_function = derivative_activation_function
+        self.learning_rate = learning_rate
+        self.output_cache = 0.0
+        self.delta = 0.0
+    
+    def output(self, inputs : List[float]) -> float:
+        self.output_cache = dot_product(inputs, self.weights)
+        return self.activation_function(self.output_cache)
