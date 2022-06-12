e9c4391ff7177d7c932b004056be3c4e211a1b0e
Beomman-Han <qjaaks6378@gmail.com>
2022-06-12 14:25:10 +0900

    Add Network class at ann.py
    Network class initializes whole layers in ann, do feed-forward, backpropagation.
    Currently, only __init__() method is implemented.

--- algorithm/classic_problem/ann.py
+++ algorithm/classic_problem/ann.py
@@ -1,7 +1,7 @@
 """This is for artificial neural network practice."""
 from __future__ import annotations
 from random import random
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, TypeVar
 from math import exp
 
 
@@ -83,3 +83,27 @@ class Layer:
             sum_weights_and_deltas : float = dot_product(next_weights, next_deltas)
             neuron.delta = neuron.derivative_activation_function(
                 neuron.output_cache) * sum_weights_and_deltas
+    
+
+T = TypeVar('T')
+
+class Network:
+    def __init__(self,
+        layer_structure : List[int],
+        learning_rate : float,
+        activation_function : Callable[[float], float] = sigmoid,
+        derivative_activation_function : Callable[[float], float] = derivative_sigmoid
+        ) -> None:
+        
+        if len(layer_structure) < 3:
+            raise ValueError('오류 : 최소 3개 이상의 층이 필요합니다 (입력층, 은닉층, 출력층)!')
+        self.layers : List[Layer] = []
+
+        input_layer : Layer = Layer(None, layer_structure[0], learning_rate,
+                        activation_function, derivative_activation_function)
+        self.layers.append(input_layer)
+        
+        for previous, num_neurons in enumerate(layer_structure[1::]):
+            next_layer = Layer(self.layers[previous], num_neurons, learning_rate,
+                        activation_function, derivative_activation_function)
+            self.layers.append(next_layer)
